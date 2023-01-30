3dce45b7199547b491a9dc7cb8e4bfc70b030623
dharmasaputraa <putudharma0@gmail.com>
2023-01-30 19:43:41 +0800

    laravel integration

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,28 @@
+    <!-- Helpers -->
+    <script src="{{ url('backend/assets/vendor/js/helpers.js') }}"></script>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="{{ url('backend/assets/js/config.js') }}"></script>
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="{{ url('backend/assets/vendor/libs/jquery/jquery.js') }}"></script>
+    <script src="{{ url('backend/assets/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ url('backend/assets/vendor/js/bootstrap.js') }}"></script>
+    <script src="{{ url('backend/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+
+    <script src="{{ url('backend/assets/vendor/js/menu.js') }}"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="{{ url('backend/assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
+
+    <!-- Main JS -->
+    <script src="{{ url('backend/assets/js/main.js') }}"></script>
+
+    <!-- Page JS -->
+    <script src="{{ url('backend/assets/js/dashboards-analytics.js') }}"></script>
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,79 @@
+<!-- Menu -->
+
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+  <div class="app-brand demo">
+    <a href="index.html" class="app-brand-link">
+      <span class="app-brand-logo demo">
+        <img src="{{ url('backend/assets/img/icons/brands/logo_nomads.png') }}" alt="" width="100">
+      </span>
+      <span class="app-brand-text demo menu-text fw-bolder ms-2">admin</span>
+    </a>
+
+    <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+      <i class="bx bx-chevron-left bx-sm align-middle"></i>
+    </a>
+  </div>
+
+  <div class="menu-inner-shadow"></div>
+  <hr class="sidebar-divider">
+  <ul class="menu-inner py-1">
+    <!-- Dashboard -->
+    <li class="menu-item active">
+      <a href="index.html" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-home-circle"></i>
+        <div data-i18n="Analytics">Dashboard</div>
+      </a>
+    </li>
+
+    <li class="menu-item">
+      <a href="index.html" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-hotel"></i>
+        <div data-i18n="Analytics">Paket Travel</div>
+      </a>
+    </li>
+
+    <li class="menu-item">
+      <a href="index.html" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-images"></i>
+        <div data-i18n="Analytics">Galery Travel</div>
+      </a>
+    </li>
+
+    <li class="menu-item">
+      <a href="index.html" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-dollar-circle"></i>
+        <div data-i18n="Analytics">transaction</div>
+      </a>
+    </li>
+
+    <li>
+      <!-- Extended components -->
+    </li>
+
+    {{-- <!-- Forms & Tables -->
+    <li class="menu-header small text-uppercase">
+      <span class="menu-header-text">Forms &amp; Tables</span>
+    </li>
+    <!-- Forms -->
+    <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon tf-icons bx bx-detail"></i>
+        <div data-i18n="Form Elements">Form Elements</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="forms-basic-inputs.html" class="menu-link">
+            <div data-i18n="Basic Inputs">Basic Inputs</div>
+          </a>
+        </li>
+        <li class="menu-item">
+          <a href="forms-input-groups.html" class="menu-link">
+            <div data-i18n="Input groups">Input groups</div>
+          </a>
+        </li>
+      </ul>
+    </li> --}}
+  </ul>
+  <hr class="sidebar-divider">
+</aside>
+<!-- / Menu -->
